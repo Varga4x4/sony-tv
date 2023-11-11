9711b5f50bbf167d22fbd5b05e11912fe8e036a8
Gabor Varga <varga4x4@outlook.com>
2023-11-11 19:17:15 +0000

    Add sub menu for Internet Contents

--- index.js
+++ index.js
@@ -1,37 +1,79 @@
 const HOME_MENU_BUTTONS_ARRAY = [
     {
-        id: "internet-contents",
-        innerText: "Internet Contents"
+        id: "internetcontents-tab",
+        innerText: "Internet Contents",
+        onclick: () => renderInternetContents()
     },
     {
-        id: "digital-programme-list",
-        innerText: "Digital Programme List"
+        id: "digital-programme-list-tab",
+        innerText: "Digital Programme List",
+        onclick: () => renderDigitalProgrammeList()
     },
     {
-        id: "digital-epg",
+        id: "digital-epg-tab",
         innerText: "Digital EPG"
     },
     {
-        id: "applications",
+        id: "applications-tab",
         innerText: "Applications"
     },
     {
-        id: "recordings",
+        id: "recordings-tab",
         innerText: "Recordings"
     },
     {
-        id: "media",
+        id: "media-tab",
         innerText: "Media"
     },
     {
-        id: "settings",
+        id: "settings-tab",
         innerText: "Settings"
     },
 ]
 
-// ELEMENTS
-const appElement = document.getElementById("app")
+const INTERNET_CONTENS_BUTTONS_ARRAY = [
+    {
+        id: "amazon-prime-video",
+        innerText: "Amazon Prime Video",
+        onclick: () => console.log("Amazon Prime Video")
+    },
+    {
+        id: "netflix",
+        innerText: "Netflix",
+        onclick: () => console.log("Netflix")
+    },
+    {
+        id: "bbc-iplayer",
+        innerText: "BBC iPlayer",
+        onclick: () => console.log("BBC iPlayer")
+    },
+    {
+        id: "youtube",
+        innerText: "YouTube",
+        onclick: () => console.log("YouTube")
+    },
+]
+
+// ELEMENT_IDS
+ELEMENT_IDS = {
+    app: "app",
+    wrapper: "wrapper",
+    internetContentsWrapper: "internet-contents-wrapper-element",
+    internetContentsTab: "internetcontents-tab",
+    programmeListTab: "digital-programme-list-tab",
+    digitalEpgTab: "digital-epg-tab",
+    applicationsTab: "applications-tab",
+    recordingsTab: "recordings-tab",
+    mediaTab: "media-tab",
+    settingsTab: "settings-tab",
+}
 //
+
+// ELEMENTS
+const appElement = document.getElementById(ELEMENT_IDS.app)
+const wrapperElement = document.getElementById(ELEMENT_IDS.wrapper)
+//
+
 
 // HELPERS
 const createElement = (tagType, attrObj, parentElement) => {
@@ -51,6 +93,23 @@ const createElement = (tagType, attrObj, parentElement) => {
 
     return newElement
 }
+
+const createGlobalWrapper = () => {
+    createElement("div", {
+        id: ELEMENT_IDS.wrapper
+    }, appElement)
+}
+
+const handleOptionsElementOnClick = (tabName) => {
+    const allTabIds = Object.entries(ELEMENT_IDS)
+        .filter(entry => entry[0].endsWith('Tab'))
+        .map(entry => entry[1])
+    const tabNameTabButtonIdValue = ELEMENT_IDS[`${tabName}Tab`]
+
+    allTabIds.forEach((tabId) => {
+        document.getElementById(tabId).disabled = tabId === tabNameTabButtonIdValue
+    })
+}
 //
 
 // RENDER_HOME_MENU
@@ -61,13 +120,46 @@ const renderHomeMenu = () => {
         innerText: "HOME"
     }, mainElement)
 
-    HOME_MENU_BUTTONS_ARRAY.forEach(({ id, innerText }) => {
+    HOME_MENU_BUTTONS_ARRAY.forEach(({ id, innerText, onclick }) => {
         createElement("button", {
             id,
             className: "home-options",
-            innerText
+            innerText,
+            onclick
         }, mainElement)
     })
+}
+//
+
+// RENDER_INTERNET_CONTENTS
+const renderInternetContents = () => {
+    // createGlobalWrapper()
+    handleOptionsElementOnClick("internetContents")
+    const wrapperElement = createElement("div", {
+        id: ELEMENT_IDS.wrapper
+    }, appElement)
+
+    const internetContentsWrapperElement = createElement("div", {
+        id: ELEMENT_IDS.internetContentsWrapper
+    }, wrapper)
+
+    INTERNET_CONTENS_BUTTONS_ARRAY.forEach(({ id, innerText, onclick }) => {
+        createElement("button", {
+            id,
+            className: "sub-options",
+            innerText,
+            onclick
+        }, internetContentsWrapperElement)
+    })
+    console.log(wrapperElement)
+}
+//
+
+// RENDER_DIGITAL_PROGRAMME_LIST
+const renderDigitalProgrammeList = () => {
+    console.log(wrapperElement)
+    handleOptionsElementOnClick("programmeList")
+    console.log("Digital Programme List")
 }
 //
 
